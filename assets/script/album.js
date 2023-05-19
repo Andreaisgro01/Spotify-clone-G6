f11b3e6ee4c1b1a9742edf1aeb381c9abd3ef2ff
Giafal <gianluca.falcone@yahoo.it>
2023-05-19 09:35:39 +0200

    play/pause funziona

--- assets/script/album.js
+++ assets/script/album.js
@@ -95,10 +95,27 @@ const appendTracks = function (datatracks) {
 const playtracks = function (track) {
   let playButton = document.getElementById("play-button");
   playButton.addEventListener("click", function () {
-    console.log("bottone cliccato");
-    console.log(track.tracks.data[0].preview);
     let audioTrack = new Audio(track.tracks.data[0].preview);
     audioTrack.play();
+    let playerPlay = document.getElementById("player-play");
+    playerPlay.classList.remove("bi-play-circle-fill");
+    playerPlay.classList.add("bi-pause-circle-fill");
+    playerPlay.innerHTML = `<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z"/>`;
+    playerPlay.addEventListener("click", function () {
+      if (playerPlay.classList.contains("bi-pause-circle-fill")) {
+        playerPlay.classList.add("bi-play-circle-fill");
+        playerPlay.classList.remove("bi-pause-circle-fill");
+        playerPlay.innerHTML = `<path
+        d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"
+        />`;
+        audioTrack.pause();
+      } else {
+        playerPlay.classList.remove("bi-play-circle-fill");
+        playerPlay.classList.add("bi-pause-circle-fill");
+        playerPlay.innerHTML = `<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z"/>`;
+        audioTrack.play();
+      }
+    });
   });
 };
 
--- assets/script/player.js
+++ assets/script/player.js
@@ -34,21 +34,21 @@ playerRepeat.addEventListener("click", function () {
   }
 });
 
-let playerPlay = document.getElementById("player-play");
+// let playerPlay = document.getElementById("player-play");
 
-playerPlay.addEventListener("click", () => {
-  if (playerPlay.classList.contains("bi-play-circle-fill")) {
-    playerPlay.classList.remove("bi-play-circle-fill");
-    playerPlay.classList.add("bi-pause-circle-fill");
-    playerPlay.innerHTML = `<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z"/>`;
-  } else {
-    playerPlay.classList.add("bi-play-circle-fill");
-    playerPlay.classList.remove("bi-pause-circle-fill");
-    playerPlay.innerHTML = `<path
-    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"
-  />`;
-  }
-});
+// playerPlay.addEventListener("click", () => {
+//   if (playerPlay.classList.contains("bi-play-circle-fill")) {
+//     playerPlay.classList.remove("bi-play-circle-fill");
+//     playerPlay.classList.add("bi-pause-circle-fill");
+//     playerPlay.innerHTML = `<path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.25 5C5.56 5 5 5.56 5 6.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C7.5 5.56 6.94 5 6.25 5zm3.5 0c-.69 0-1.25.56-1.25 1.25v3.5a1.25 1.25 0 1 0 2.5 0v-3.5C11 5.56 10.44 5 9.75 5z"/>`;
+//   } else {
+//     playerPlay.classList.add("bi-play-circle-fill");
+//     playerPlay.classList.remove("bi-pause-circle-fill");
+//     playerPlay.innerHTML = `<path
+//     d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM6.79 5.093A.5.5 0 0 0 6 5.5v5a.5.5 0 0 0 .79.407l3.5-2.5a.5.5 0 0 0 0-.814l-3.5-2.5z"
+//   />`;
+//   }
+// });
 
 //buongiorno/buonasera
 
@@ -63,7 +63,6 @@ function times() {
   }
   return word;
 }
-
 
 let answer = document.getElementById("goodmorning");
 
