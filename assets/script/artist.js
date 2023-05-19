e15ed6e669e44980c917899e83bab0fb866c13e0
andbardii <andbardii@icloud.com>
2023-05-19 15:07:55 +0200

    add some coll

--- assets/script/artist.js
+++ assets/script/artist.js
@@ -53,14 +53,14 @@ const fillHeader = (data) => {
   favouriteSongs.innerHTML = "";
 
   let headFiller = `
-    <p class="pt-5 mb-0">
+    <p class="pt-5 mb-0 d-none d-lg-block">
         <svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="#3E91F4" class="bi bi-patch-check-fill" viewBox="0 0 16 16">
         <path d="M10.067.87a2.89 2.89 0 0 0-4.134 0l-.622.638-.89-.011a2.89 2.89 0 0 0-2.924 2.924l.01.89-.636.622a2.89 2.89 0 0 0 0 4.134l.637.622-.011.89a2.89 2.89 0 0 0 2.924 2.924l.89-.01.622.636a2.89 2.89 0 0 0 4.134 0l.622-.637.89.011a2.89 2.89 0 0 0 2.924-2.924l-.01-.89.636-.622a2.89 2.89 0 0 0 0-4.134l-.637-.622.011-.89a2.89 2.89 0 0 0-2.924-2.924l-.89.01-.622-.636zm.287 5.984-3 3a.5.5 0 0 1-.708 0l-1.5-1.5a.5.5 0 1 1 .708-.708L7 8.793l2.646-2.647a.5.5 0 0 1 .708.708z"/>
         </svg>
         Artista Verificato
     </p>
-  <h2 class="artistName mb-3">${data.name}</h1>
-  <p class="py-2">${data.nb_fan} followers</p>
+  <h2 class="artistName p-2 mb-3">${data.name}</h1>
+  <p class="py-2 d-none d-lg-block">${data.nb_fan} followers</p>
     `;
   artistHeader.innerHTML += headFiller;
 
@@ -97,13 +97,13 @@ const popularFill = (popular) => {
     targetSong.innerHTML += `
         <div class="col-8 mt-5">
             <span>${popularCounter}</span>
-            <img src="${element.album.cover}" alt="foto">
+            <img width="70" class="px-2" src="${element.album.cover}" alt="foto">
             <span>${element.title}</span>
         </div>
-        <div class="col-3">
+        <div class="col-3 p-0">
             <span>${element.rank}</span>
         </div>
-        <div class="col-1">
+        <div class="col-1 p-0">
             <span>${Math.floor(element.duration / 60)}:${
       element.duration % 60
     }</span>
